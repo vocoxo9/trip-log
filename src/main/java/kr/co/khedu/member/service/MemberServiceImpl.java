b316084e50db71880024fad49686d5c80de2e6fd
vocoxo9 <vocoxo9@gmail.com>
2025-04-07 15:43:40 +0900

    회원가입 기능 구현-young

---
+++ src/main/java/kr/co/khedu/member/service/MemberServiceImpl.java
@@ -0,0 +1,44 @@
+package kr.co.khedu.member.service;
+
+import org.apache.ibatis.session.SqlSession;
+
+import kr.co.khedu.member.model.dao.MemberDAO;
+import kr.co.khedu.member.model.vo.Member;
+import kr.co.khedu.template.Template;
+
+public class MemberServiceImpl implements MemberService{
+	private MemberDAO mDAO = new MemberDAO();
+	
+	
+	@Override
+	public int insertMember(Member member) {
+		SqlSession sqlSession = Template.getSqlSession();
+		int result = mDAO.insertMember(sqlSession, member);
+		if(result > 0) {
+			sqlSession.commit();
+		}
+		sqlSession.close();
+		return result;
+	}
+
+	@Override
+	public Member selectMember() {
+		SqlSession sqlSession = Template.getSqlSession();
+		Member m = mDAO.selectMember(sqlSession);
+		sqlSession.close();
+		return m;
+	}
+
+	@Override
+	public int updateMember(Member member) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteMember(int memNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
