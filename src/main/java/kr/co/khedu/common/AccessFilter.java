33b701f7a7b7c2bda3140cee3c633f1068737d88
yoonseo0832 <yoonseo0832@gmail.com>
2025-04-12 22:29:36 +0900

    로그인페이지에 돌아기기 버튼 수정-yoon

--- src/main/java/kr/co/khedu/common/AccessFilter.java
+++ src/main/java/kr/co/khedu/common/AccessFilter.java
@@ -21,7 +21,7 @@ public class AccessFilter implements Filter{
 	// 접근 허용할 url들
 	private static final List<String> EXCLUDED_URLS = 
 			Arrays.asList("/trip-log/", "/trip-log/members/sign-in","/trip-log/members/sign-up",
-					"/trip-log/products", "/trip-log/auth/sign-in", "/trip-log/google-login/callback?" );
+					"/trip-log/products", "/trip-log/auth/sign-in", "/trip-log/google-login/callback?");
 	
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
